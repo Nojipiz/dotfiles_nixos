5d77fc63991b654d790059088b53f8f215b4bad0
Orlando Vargas <nojipiz@protonmail.com>
2023-07-02 13:06:55 -0500

    removed: MacOs Virtualization!

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -33,19 +33,6 @@ in
   networking.hostName = "OLap"; # Define your hostname.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-
-  # TEST MACOS -- Remove if doesn't work
-  boot.extraModprobeConfig = ''
-    options kvm_intel nested=1
-    options kvm_intel emulate_invalid_guest_state=0
-    options kvm ignore_msrs=1
-  '';
-  virtualisation.libvirtd.enable = true;
-  programs.dconf.enable = true;
-  # END TEST MACOS -- Remove if doesn't work
-
-
-
   # Set your time zone.
   time.timeZone = "America/Bogota";
   environment.sessionVariables = rec {
@@ -174,15 +161,6 @@ in
     php74
     composer
     nodejs
-
-    # TEST MACOS
-    qemu
-    libvirt
-    dnsmasq
-    virt-manager
-    bridge-utils
-    flex
-    bison
   ];
 
   fonts.fonts = with pkgs; [
