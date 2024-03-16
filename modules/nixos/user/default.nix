8d5468fbdd028e9bc3ccc4e6615040f01b4f6e6a
Orlando Vargas <nojipiz@protonmail.com>
2024-03-16 14:17:55 -0500

    fix: JVM garbage collector type for Scala

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -12,7 +12,7 @@
     EDITOR = "nvim";
     BROWSER = "chromium";
     TERMINAL = "alacritty";
-    SBT_OPTS="-Xmx5G -XX:+UseConcMarkSweepGC -XX:+IgnoreUnrecognizedVMOptions -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=3G -Xss2M  -Duser.timezone=GMT";
+    SBT_OPTS="-Xmx5G -XX:+UseParallelGC -XX:+IgnoreUnrecognizedVMOptions -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=3G -Xss4M -Duser.timezone=GMT";
     JAVA_OPTS="-Xmx10G";
     COMPOSER_MEMORY_LIMIT="-1";
   };
