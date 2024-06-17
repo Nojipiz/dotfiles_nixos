f2b1595f9182e4cb7b6b0c2eaf3aae4641302bb6
Orlando Vargas <nojipiz@protonmail.com>
2024-06-16 20:23:46 -0500

    feature: Allowing more ports

--- modules/nixos/networking/default.nix
+++ modules/nixos/networking/default.nix
@@ -7,9 +7,11 @@
 
     networking.firewall = {
       enable = true;
-      allowedTCPPorts = [ 80 443 19000 3000 8090];
+      allowedTCPPorts = [ 80 443 19000 ];
       allowedTCPPortRanges = [
-      { from = 30000 ; to = 46000; }
+        { from = 30000 ; to = 46000; }
+        { from = 3000 ; to = 4000; }
+        { from = 8000; to = 9000; }
       ];
     };
   };
