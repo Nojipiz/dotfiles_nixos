61e4be0cfe02282fab6fb260c6dc625b8e0e9132
Orlando Vargas <nojipiz@protonmail.com>
2024-06-18 21:30:12 -0500

    feature: Extra

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -12,9 +12,9 @@
     EDITOR = "nvim";
     BROWSER = "chromium";
     TERMINAL = "alacritty";
-    SBT_OPTS = "-Xmx5G -XX:+UseG1GC -XX:+IgnoreUnrecognizedVMOptions -XX:MaxPermSize=4G -Xss4M -Duser.timezone=GMT";
+    TERM = "xterm-color";
+    SBT_OPTS = "-Xmx6G -XX:+UseG1GC -XX:+IgnoreUnrecognizedVMOptions -XX:MaxPermSize=4G -Xss4M -Duser.timezone=GMT";
     JAVA_OPTS = "-Xmx10G -XX:+UseG1GC -XX:+UseStringDeduplication";
     COMPOSER_MEMORY_LIMIT = "-1";
-    #LD_LIBRARY_PATH = nixpkgs.zlib/lib;
   };
 }
