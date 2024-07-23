0432fb746b76e0d71dd0184005ae6001d90fa002
Orlando Vargas <nojipiz@protonmail.com>
2024-07-23 07:34:23 -0500

    feat: fish integration with starship

--- home-manager/base/server/terminal/default.nix
+++ home-manager/base/server/terminal/default.nix
@@ -1,17 +1,41 @@
 { pkgs, ... }:
 {
+  programs.nix-index.enableFishIntegration = true;
   programs.fish = {
     enable = true;
+    shellInit = ''
+      set fish_greeting '''
+    '';
     interactiveShellInit = ''
-      starship init fish | source
-      set fish_greeting
-      alias g="lazygit"
-      alias v="lvim"
-      alias graalvmjava=${pkgs.graalvm-ce}/bin/java
       fish_add_path ~/mutable_node_modules/bin
       fish_add_path ~/.local/bin
     '';
+    shellAliases = {
+      g = "lazygit";
+      v ="lvim";
+      graalvmjava = "${pkgs.graalvm-ce}/bin/java";
+    };
   };
 
-  programs.nix-index.enableFishIntegration = true;
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = { 
+      gcloud = {
+        format = "[$symbol]($style)";
+        symbol = "☁️ ";
+        style = "bold blue";
+      };
+
+      java = {
+        detect_extensions = ["java" "cljs" "cljc"];
+      };
+
+      scala = {
+        format = "[$\{symbol} ($\{version} )]($style)";
+        symbol = "Scala";
+        style = "bold red dimmed";
+      };
+    };
+  };
 }
