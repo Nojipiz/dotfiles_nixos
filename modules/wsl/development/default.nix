f1ac50d8603076fce595d2886af9951c68f30510
Orlando Vargas <nojipiz@protonmail.com>
2024-09-04 23:03:35 -0500

    feat: vpmwsl

--- modules/wsl/development/default.nix
+++ modules/wsl/development/default.nix
@@ -3,7 +3,6 @@
   environment.systemPackages = with pkgs; [
     lunarvim
     unstable.zed-editor
-    wsl-vpnkit
     gradle
 
     # Infrastructure
--- modules/wsl/networking/default.nix
+++ modules/wsl/networking/default.nix
@@ -1,6 +1,26 @@
+{ pkgs, ... }:
 {
-  config = {
+  environment.systemPackages = with pkgs; [
+    wsl-vpnkit
+  ];
 
+  environment.shellAliases = {
+    vpn-start = "sudo systemctl start wsl-vpnkit";
+    vpn-stop = "sudo systemctl stop wsl-vpnkit";
+    vpn-status = "systemctl status wsl-vpnkit";
+  };
+  systemd.services.wsl-vpnkit = {
+    enable = true;
+    description = "wsl-vpnkit";
+    serviceConfig = {
+      ExecStart = "${pkgs.unstable.wsl-vpnkit}/bin/wsl-vpnkit";
+      Type = "idle";
+      Restart = "always";
+      KillMde = "mixed";
+    };
+  };
+
+  config = {
     networking = {
       hostName = "OLap"; 
       networkmanager.enable = true; 
