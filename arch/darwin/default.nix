da071a41e1f7953e4cb5bb80cb73771348e31691
Orlando Vargas <nojipiz@protonmail.com>
2025-01-06 16:27:54 -0500

    fix: Self not found

--- arch/darwin/default.nix
+++ arch/darwin/default.nix
@@ -1,4 +1,4 @@
-{ self, pkgs, ... }: {
+{ pkgs, ... }: {
   environment.systemPackages =
     [ 
       pkgs.vim
@@ -11,7 +11,7 @@
   services.nix-daemon.enable = true;
   nix.settings.experimental-features = "nix-command flakes";
   programs.zsh.enable = true;  # default shell on catalina
-  system.configurationRevision = self.rev or self.dirtyRev or null;
+  # system.configurationRevision = self.rev or self.dirtyRev or null;
   system.stateVersion = 4;
   nixpkgs.hostPlatform = "aarch64-darwin";
   security.pam.enableSudoTouchIdAuth = true;
