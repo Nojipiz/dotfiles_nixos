39d0c99fbb5b75dfa0845d7e23423927eb20dc3f
Orlando Vargas <nojipiz@protonmail.com>
2025-02-09 15:32:38 -0500

    feat: Added extra config to ideavim

--- common/home/jetbrains/default.nix
+++ common/home/jetbrains/default.nix
@@ -1,4 +1,9 @@
 {
+
+# NOTE: Intellij require extra steps to works as expected.
+# - In tree, select Settings -> Always Select Opened File [Or any other to open the tree in the current file]
+# - Remove keymap in Settings -> Keymap: "Enable Preview Tab"
+
   home.file.".ideavimrc" = {
     source = ./ideavimrc;
   };
