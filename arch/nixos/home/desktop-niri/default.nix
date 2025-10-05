ed114883e53dbdd8a3bfb4acbe8d5648674f5d54
nojipiz@protonmail.com <nojipiz@protonmail.com>
2025-10-05 10:31:02 -0500

    feat: Niri start file

---
+++ arch/nixos/home/desktop-niri/default.nix
@@ -0,0 +1,26 @@
+
+let
+  wallpapers = [
+    ../../../../media/wallpaper/initiald.jpg
+    ../../../../media/base_wallpaper.png
+  ];
+in {
+  imports = [
+    ../../../../common/home
+
+    ./niri
+    ./waybar
+  ];
+
+  home = {
+    username = "nojipiz";
+    homeDirectory = "/home/nojipiz";
+    stateVersion = "25.05";
+  };
+
+  home.file = {
+    "Images/wallpaper/base_wallpaper.png" = {
+      source = builtins.head wallpapers;
+    };
+  };
+}
